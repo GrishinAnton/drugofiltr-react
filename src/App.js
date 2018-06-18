d2cc96a9a8816627f51ad38c171d856bd37e0b9d
GrishinAnton <grishin_rostov@mail.ru>
2018-06-18 21:34:54 +0300

    Настройка, сборка

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'css/style.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="container-wrapper">
+        <div className="filter-header flex flex_jc-sb flex_a-c p">
+          <p className="filter-header__title">Выберите друзей</p>
+          <button className="filter-header__close button-reset"></button>
+        </div>
+        <div className="filer-search flex flex_jc-sb flex_a-c p">
+          <input className="input_img input-friends-vk" type="text" placeholder="Начните вводить имя друга" />
+          <input className="input-friends-save" type="text" placeholder="Название" />
+        </div>
+        <div className="filter-body flex flex_jc-sb p">
+          <div className="left-column">
+            <p className="filter-body__title">Ваши друзья</p>
+            <div className="friends-wrapper"></div>
+          </div>
+          <div className="right-column">
+            <p className="filter-body__title">Друзья в списке</p>
+            <div className="friends-wrapper"></div>
+          </div>
+        </div>
+        <div className="filter-footer p flex flex_a-c">
+          <button className="button-save js-button">Сохранить</button>
+        </div>
+    </div>
     );
   }
 }
